da3d495536e0293b80fbb9b4330c58c45cda3a7f
ljmacleanferrand <87734454+ljmacleanferrand@users.noreply.github.com>
2021-11-01 13:06:06 -0700

    about page map render

--- page-about.php
+++ page-about.php
@@ -30,7 +30,6 @@ get_header();
 				if ( $about_img ) :
 				?>
   				  <img id="about-hero" src="<?php echo esc_url( $about_img['url'] ); ?>" alt="<?php echo esc_attr( $about_img['alt'] ); ?>" />
-
     			<?php 
 				endif; 
 
@@ -38,20 +37,66 @@ get_header();
 				if ( $owner_img ) :
 				?>
    				 <img id="owner-img" src="<?php echo esc_url( $owner_img['url'] ); ?>" alt="<?php echo esc_attr( $owner_img['alt'] ); ?>" />
-
   			    <?php 
 				endif; 
 
+				if ( get_field('about_owners') ) :
+					?>
+					<p> <?php the_field( 'about_owners' ); ?> </p> 
+					<?php 
+				endif;
+
 			
+		
+				// ACF Google Map Single Map Output
+			
+				
 				$location = get_field('main_office_map');
 				if( $location ): ?>
+
+				<!-- ACF Styles per the Docs -->
+				<style type="text/css">
+				.acf-map {
+					width: 100%;
+					height: 400px;
+					border: #ccc solid 1px;
+					margin: 20px 0;
+				}
+
+
+				.acf-map img {
+				max-width: inherit !important;
+				}
+				</style>
+
 					<div class="acf-map" data-zoom="16">
 						<div class="marker" data-lat="<?php echo esc_attr($location['lat']); ?>" data-lng="<?php echo esc_attr($location['lng']); ?>"></div>
 					</div>
-			<?php
+				<?php 
+				endif;
+				
+
+				$location = get_field('main_office_map');
+					if( $location ) {
+
+						// Loop over segments and construct HTML.
+						$address = '';
+						foreach( array('street_number', 'street_name', 'city', 'state', 'post_code', 'country') as $i => $k ) {
+							if( isset( $location[ $k ] ) ) {
+								$address .= sprintf( '<span class="segment-%s">%s</span>, ', $k, $location[ $k ] );
+							}
+						}
+
+						// Trim trailing comma.
+						$address = trim( $address, ', ' );
+
+						// Display HTML.
+						echo '<p>' . $address . '.</p>';
+					}
+
+
 			endif;
-			endif;
-			endwhile; ?>
+		endwhile; ?>
 
 </main><!-- #main -->
 
