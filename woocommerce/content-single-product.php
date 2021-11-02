01739eb1940952be0489b840a183fe2905671fbd
nicoledawn <87670272+nicoledawn92@users.noreply.github.com>
2021-11-02 10:57:59 -0700

    upodate functions
    need to add this info to functions file

--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -1,4 +1,10 @@
 <?php
+
+remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_title', 5);
+remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_price', 10);
+add_action('woocommerce_before_single_product_summary', 'woocommerce_template_single_title', 5);
+add_action('woocommerce_single_product_summary', 'woocommerce_output_product_data_tabs', 20);
+remove_action('woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10);
 /**
  * The template for displaying product content in the single-product.php template
  *
