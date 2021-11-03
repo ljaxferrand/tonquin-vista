4b556fd5cef8cf2ecf2acabeadfb5fecb66ecf63
ljmacleanferrand <87734454+ljmacleanferrand@users.noreply.github.com>
2021-11-03 09:20:35 -0700

    multiple map marker rendering minus functions.php

---
+++ inc/cabins-category-hooks.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * Display category image as a Hero on category archive
+ */
+add_action( 'woocommerce_archive_description', 'woocommerce_category_image', 2 );
+function woocommerce_category_image() {
+    if ( is_product_category() ){
+	    global $wp_query;
+	    $cat = $wp_query->get_queried_object();
+	    $thumbnail_id = get_term_meta( $cat->term_id, 'thumbnail_id', true );
+	    $image = wp_get_attachment_url( $thumbnail_id );
+	    if ( $image ) {
+		    echo '<img src="' . $image . '" alt="' . $cat->name . '" />';
+		}
+	}
+}       
+
+// Add map to the Cabins Product Category Page
+
+add_action( 'woocommerce_after_main_content', 'add_map' );
+function add_map() {
+    if ( is_product_category('cabins') )        {
+        $args = array(
+            'post_type'             => 'product',
+            'post_status'           => 'publish',
+            'posts_per_page'        => -1,
+            'tax_query'             => array(
+                array(
+                    'taxonomy'      => 'product_cat',
+                    'field' => 'term_id', 
+                    'terms'         => 24,
+                    'operator'      => 'IN',
+                ),
+            )
+        );
+        $products = new WP_Query($args);
+
+        echo "<div class='map-container'><div class='wrap'><div class='acf-map'>";
+
+        while ( $products->have_posts() ) : $products->the_post();
+
+            $location = get_field('single_cabin_location');
+            $title = get_the_title(); // Get the title
+
+                if( !empty($location) ) {
+                ?>
+                    <div class="marker" data-lat="<?php echo $location['lat']; ?>" data-lng="<?php echo $location['lng']; ?>">
+                                <h4><a href="<?php the_permalink(); ?>" rel="bookmark"> <?php the_title(); ?></a></h4>
+                                <p class="address"><?php echo $location['address']; ?></p>
+                    </div>
+                <?php
+                }
+        endwhile;
+
+        echo '</div></div></div>';
+        wp_reset_postdata();
+    }
+}
--- page-about.php
+++ page-about.php
@@ -44,71 +44,63 @@ get_header();
 					?>
 					<p> <?php the_field( 'about_owners' ); ?> </p> 
 					<?php 
-				endif;
-
-			
-				// For "Getting Here" info tab -----------------------------
-				// ACF Google Map Single Map Output
-				
-				$location = get_field('main_office_map');
-				if( $location ): ?>
-
-				<!-- ACF Styles per the Docs -->
-				<style type="text/css">
-				.acf-map {
-					width: 100%;
-					height: 400px;
-					border: #ccc solid 1px;
-					margin: 20px 0;
-				}
-
-				.acf-map img {
-				max-width: inherit !important;
-				}
-				</style>
-
-					<h2>Getting Here</h2>
-					<div class="acf-map" data-zoom="16">
-						<div class="marker" data-lat="<?php echo esc_attr($location['lat']); ?>" data-lng="<?php echo esc_attr($location['lng']); ?>"></div>
-					</div>
-				<?php 
 				endif; ?>
-				
-					<!-- For "Contact Info" info tab -------------------------------- -->
 
-					<h2>Contact Us</h2>
-
-					<?php if( have_rows('address', 125) ): ?>
-						<?php while( have_rows('address', 125) ): the_row(); 
-
-							// Get sub field values.
-							$addressline1 = get_sub_field('street_address');
-							$addressline2 = get_sub_field('address_details');
-							$phone = get_sub_field('phone_number');
-
-							?>
-								<div id="contact-address">
-										<p><?php echo $addressline1 ?></p>
-										<p><?php echo $addressline2 ?></p>
-										<p><?php echo $phone ?></p>
+				<!-- Info Tabs -->
+				<div id="explore-experiences">
+				<ul id="nav-tab" class="nav-tab-ul">
+                        <li class="active"><a href="#gettinghere">Getting Here</a></li>
+                        <li><a href="#contactinfo">Contact Info</a></li>
+						<li><a href="#localweather">Local Weather</a></li>
+                </ul>
+			
+					<div id="tab-content">
+						<div class='tab-pane active' id="gettinghere">
+							<?php
+							$location = get_field('main_office_map');
+							if( $location ): ?>
+			
+								<div class="acf-map" data-zoom="16">
+									<div class="marker" data-lat="<?php echo esc_attr($location['lat']); ?>" data-lng="<?php echo esc_attr($location['lng']); ?>"></div>
 								</div>
-						<?php endwhile; ?>
-					<?php endif;
+							<?php 
+							endif; ?>
+						</div>
 
-					if ( get_field('contact_page_link') ) :
-						?>
-						<button><a href="<?php the_field( 'contact_page_link' ); ?>"> Visit our Contact Page</a></button>
-						<?php 
-					endif; ?>
+						<div class='tab-pane' id="contactinfo">
+			
+								<?php if( have_rows('address', 125) ): ?>
+									<?php while( have_rows('address', 125) ): the_row(); 
+			
+										// Get sub field values.
+										$addressline1 = get_sub_field('street_address');
+										$addressline2 = get_sub_field('address_details');
+										$phone = get_sub_field('phone_number');
+			
+										?>
+											<div id="contact-address">
+													<p><?php echo $addressline1 ?></p>
+													<p><?php echo $addressline2 ?></p>
+													<p><?php echo $phone ?></p>
+											</div>
+									<?php endwhile; ?>
+								<?php endif;
+			
+								if ( get_field('contact_page_link') ) :
+									?>
+									<button><a href="<?php the_field( 'contact_page_link' ); ?>"> Visit our Contact Page</a></button>
+									<?php 
+								endif; ?>
+						</div>
 
-					<!-- For "Local Weather" info tab------------------------------------------- -->
+						<div class='tab-pane' id="localweather">
 
-					<h2>Local Weather</h2>
-
-					<?php
-					// Weather Atlas Widget shortcode
-					echo do_shortcode( '[shortcode-weather-atlas city_selector=325344 layout="horizontal" background_color="#637368" daily=5 unit_c_f="c"]' ); ?>
-
+							<?php
+							// Weather Atlas Widget shortcode
+							echo do_shortcode( '[shortcode-weather-atlas city_selector=325344 layout="horizontal" background_color="#637368" daily=5 unit_c_f="c"]' ); ?>
+						</div>	
+					</div>
+				</div>	
 			<?php
 			endif;
 		endwhile; ?>
