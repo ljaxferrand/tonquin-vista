20e9dc208128f08afa198450d16c440966f8933a
Chris Coulson <chrisjamescoulson@gmail.com>
2021-11-08 13:25:10 -0800

    Added SCSS files, added variables, used variables in scss files

--- functions.php
+++ functions.php
@@ -142,6 +142,14 @@ add_action( 'widgets_init', 'tonquin_vista_widgets_init' );
  * Enqueue scripts and styles.
  */
 function tonquin_vista_scripts() {
+
+	wp_enqueue_style(
+		'tonquin-vista-googlefonts',
+		'https://fonts.googleapis.com/css2?family=Megrim&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap',
+		array(),
+		null
+	);
+
 	wp_enqueue_style( 'tonquin-vista-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_style_add_data( 'tonquin-vista-style', 'rtl', 'replace' );
 
